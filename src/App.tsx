daa03c3421395293a90d19da270160486afd27c7
Andy Pataki <anpataki@gmail.com>
2026-04-28 16:12:53 +0200

    Added Dockerfile.prod and docker-compose.yml; Running dev and building prod is working as intended (see readme.md)

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
+          <p>Hello, World!</p>
+          <p>Test 3</p>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
